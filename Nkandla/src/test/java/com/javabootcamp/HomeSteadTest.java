b114f693ff5075a0fec655bf5b9c28d8992018a0
Abrie van Aardt <vanaardta@bbd.co.za>
2017-03-15 09:45:41 +0200

    Added mockito dependency and Visitor interface

--- Nkandla/src/test/java/com/javabootcamp/HomeSteadTest.java
+++ Nkandla/src/test/java/com/javabootcamp/HomeSteadTest.java
@@ -2,49 +2,56 @@ package com.javabootcamp;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-public class HomeSteadTest 
+public class HomeSteadTest
 {
     HomeStead homeStead;
-    
+
     @Before
     public void createInstance()
     {
         homeStead = new HomeStead();
     }
-    
-    
+
+
     @Test
     public void testHomeSteadConstructor()
     {
-        assertEquals(homeStead.getClass(), HomeStead.class);  
+        assertEquals(homeStead.getClass(), HomeStead.class);
     }
-    
+
     @Test
     public void testNameGetterAndSetter()
     {
         homeStead.setName("TestName");
         assertEquals(homeStead.getName(), "TestName");
     }
-    
+
     @Test
     public void testDistrictGetterAndSetter()
     {
         homeStead.setDistrict("TestDistrict");
         assertEquals(homeStead.getDistrict(), "TestDistrict");
     }
-    
+
     @Test
     public void testProvinceGetterAndSetter()
     {
         homeStead.setProvince("TestProvince");
         assertEquals(homeStead.getProvince(), "TestProvince");
     }
-    
+
     @Test
     public void testCountryGetterAndSetter()
     {
         homeStead.setCountry("TestCountry");
         assertEquals(homeStead.getCountry(), "TestCountry");
+    }
+
+    @Test
+    public void testAccept()
+    {
+        //mock the visitor interface        
     }
 }
